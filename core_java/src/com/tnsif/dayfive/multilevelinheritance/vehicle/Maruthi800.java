26fbfc511d30216bed0093330fcbea9151fa357e
haribalaji987-cloud <haribalaji987@gmail.com>
2025-10-07 21:07:05 +0530

    day 5 commited

---
+++ core_java/src/com/tnsif/dayfive/multilevelinheritance/vehicle/Maruthi800.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayfive.multilevelinheritance.vehicle;
+
+public class Maruthi800 {
+
+	
+		public Maruti800() {
+			System.out.println("Maruti Model: 800");
+		}
+
+		public void speed() {
+			System.out.println("Max: 80Kmph");
+		}
+
+	}
+
+}
