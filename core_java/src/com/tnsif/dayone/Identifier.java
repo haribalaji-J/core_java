651b5b5c2364fb1697c32743de7f1e2902225e13
haribalaji987-cloud <haribalaji987@gmail.com>
2025-10-07 20:22:43 +0530

    day 1 and 2 commited

---
+++ core_java/src/com/tnsif/dayone/Identifier.java
@@ -0,0 +1,14 @@
+package com.tnsif.dayone;
+
+public class Identifier {
+
+	public static void main(String[] args) {
+		int $number = 21; //starts with $
+        System.out.println("value of the number variable is : "+ $number);
+
+        String studentName="Adinaya"; //use camelcase
+        System.out.println("value is : "+ studentName);
+
+	}
+
+}
---
+++ core_java/src/com/tnsif/dayone/programone.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+
+public class programone {
+
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Java");	}
+
+	}
+
+}
