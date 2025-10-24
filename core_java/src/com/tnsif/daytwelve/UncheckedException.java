5b579c3291bd1cd7a24c90d1b6afc5adec2230ff
haribalaji987-cloud <haribalaji987@gmail.com>
2025-10-24 21:36:51 +0530

    day 12 commited

---
+++ core_java/src/com/tnsif/daytwelve/UncheckedException.java
@@ -0,0 +1,9 @@
+package com.tnsif.daytwelve;
+public class UncheckedException {
+	public static void main(String[] args) {
+		int intArray[];
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[6]);
+		
+	}
+}
